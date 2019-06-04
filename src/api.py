9030dabdbe2d773206a8647b12897302b52a8976
Param Thakkar <pht0104@gmail.com>
2019-06-03 21:44:38 -0400

    Create Flask app

---
+++ src/api.py
@@ -0,0 +1,28 @@
+import flask
+from flask import Response, request
+from src.main import main
+from werkzeug.exceptions import BadRequest
+from youtube_transcript_api import YouTubeTranscriptApi as ytcc
+import random
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.errorhandler(BadRequest)
+def handle_invalid_ytid(e):
+	return Response("{'status': 400, 'reason': A valid YouTube video ID has not been provided}", status = 400, mimetype = 'application/json')
+
+@app.errorhandler(ytcc.CouldNotRetrieveTranscript)
+def handle_missing_caption(e):
+	return Response("{'status': 400, 'reason': The video either does not exist or does not have captions enabled}", status = 400, mimetype = 'application/json')
+
+@app.route('/', methods=['GET'])
+def home():
+	vid = request.args["video_id"]
+	out = f"out/{vid[-11:]}.png"
+	wordcloud = main(vid, out)
+	filepath = request.base_url + wordcloud
+	return Response(filepath, status = 201, mimetype = 'application/json')
+
+
+app.run()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from src.utilities import get_video_id, generate_word_cloud as word_cloud, get_captions as get_cc
 
 def main(url: str, filepath="out/test.png"):
-    video_id = get_video_id(url)
-    captions = get_cc(video_id)
-    image = word_cloud(captions)
-    image.save(filepath)
+	video_id = get_video_id(url)
+	captions = get_cc(video_id)
+	image = word_cloud(captions)
+	image.save(filepath)
+	return filepath
 
 if __name__ == "__main__":
-    print("Please enter the link to a YouTube video")
-    main(input())
+	print("Please enter the link to a YouTube video")
+	main(input())
