d03276d8134c204da13942bb0ea875c0c38a9095
Param Thakkar <pht0104@gmail.com>
2019-06-08 19:46:17 -0400

    Debug server timeout

--- api.py
+++ api.py
@@ -1,19 +1,19 @@
 import flask
 from flask import Response, request
 # from src.main import main
-from werkzeug.exceptions import BadRequest
-from youtube_transcript_api import YouTubeTranscriptApi as ytcc
+# from werkzeug.exceptions import BadRequest
+# from youtube_transcript_api import YouTubeTranscriptApi as ytcc
 import random
 
 app = flask.Flask(__name__)
 
-@app.errorhandler(BadRequest)
-def handle_invalid_ytid(e):
-	return Response("{'status': 400, 'reason': A valid YouTube video ID has not been provided}", status = 400, mimetype = 'application/json')
+# @app.errorhandler(BadRequest)
+# def handle_invalid_ytid(e):
+# 	return Response("{'status': 400, 'reason': A valid YouTube video ID has not been provided}", status = 400, mimetype = 'application/json')
 
-@app.errorhandler(ytcc.CouldNotRetrieveTranscript)
-def handle_missing_caption(e):
-	return Response("{'status': 400, 'reason': The video either does not exist or does not have captions enabled}", status = 400, mimetype = 'application/json')
+# @app.errorhandler(ytcc.CouldNotRetrieveTranscript)
+# def handle_missing_caption(e):
+# 	return Response("{'status': 400, 'reason': The video either does not exist or does not have captions enabled}", status = 400, mimetype = 'application/json')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
