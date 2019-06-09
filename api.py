2fb1a3ad8f374bc5605a3609d2f00a21a6a2de85
Restyled.io <commits@restyled.io>
2019-06-09 00:18:07 +0000

    Restyled by autopep8

--- api.py
+++ api.py
@@ -7,24 +7,28 @@ import random
 
 app = flask.Flask(__name__)
 
+
 @app.errorhandler(BadRequest)
 def handle_invalid_ytid(e):
-	return Response("{'status': 400, 'reason': A valid YouTube video ID has not been provided}", status = 400, mimetype = 'application/json')
+    return Response("{'status': 400, 'reason': A valid YouTube video ID has not been provided}", status=400, mimetype='application/json')
+
 
 @app.errorhandler(ytcc.CouldNotRetrieveTranscript)
 def handle_missing_caption(e):
-	return Response("{'status': 400, 'reason': The video either does not exist or does not have captions enabled}", status = 400, mimetype = 'application/json')
+    return Response("{'status': 400, 'reason': The video either does not exist or does not have captions enabled}", status=400, mimetype='application/json')
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home(path):
-	try:
-		vid = request.args["video_id"]
-		out = f"out/{vid[-11:]}.png"
-		wordcloud = main(vid, out)
-		filepath = request.base_url + wordcloud
-		return Response(f"{{'status': 201, 'video': {vid}, 'wordcloud': {filepath}}}", status = 201, mimetype = 'application/json')
-	except Exception as e:
-		return Response(f"Error 502: {e}", status = 502, mimetype = 'application/json')
+    try:
+        vid = request.args["video_id"]
+        out = f"out/{vid[-11:]}.png"
+        wordcloud = main(vid, out)
+        filepath = request.base_url + wordcloud
+        return Response(f"{{'status': 201, 'video': {vid}, 'wordcloud': {filepath}}}", status=201, mimetype='application/json')
+    except Exception as e:
+        return Response(f"Error 502: {e}", status=502, mimetype='application/json')
+
 
 app.run()
