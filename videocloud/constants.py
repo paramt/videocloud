db124fdcce7a0946f1486429964f4ac15b84ae74
Param Thakkar <pht0104@gmail.com>
2019-06-18 16:15:44 -0400

    Merge constants and utilities into main script

--- videocloud/constants.py
+++
@@ -1,3 +0,0 @@
-# Use ArialUnicode/ArialUnicode.ttf for global language support
-FONT    = "NotoSans/NotoSans.ttf"
-BG      = "#d1d1d1"
--- videocloud/main.py
+++ videocloud/main.py
@@ -1,20 +1,46 @@
 import os
 import sys
-from videocloud.utilities import get_video_id, generate_word_cloud as word_cloud, get_captions as get_cc
+from PIL.Image import Image
+from wordcloud import WordCloud
 from youtube_transcript_api import YouTubeTranscriptApi as ytcc
+
+def get_video_id(url: str) -> str:
+    return(url[-11:])
+
+def get_cc(video_id: str) -> str:
+	"""Returns captions from a YouTube video in a word-separated list"""
+	captions = ytcc.get_transcript(video_id)
+	full_captions = ""
+
+	for caption in captions:
+		full_captions += caption["text"] + " "
+
+	return full_captions.lower()
+
+def wordcloud(words: str) -> Image:
+	"""Generates a word cloud from a list of strings"""
+	wordcloud = WordCloud(
+		width = 1000,
+		height = 500,
+		# font_path = "assets/fonts/NotoSans/NotoSans.ttf",
+		background_color = "#d1d1d1").generate(words)
+
+	image = wordcloud.to_image()
+	return image
 
 
 def videocloud(url: str, filepath):
 	try:
 		video_id = get_video_id(url)
 		captions = get_cc(video_id)
-		image = word_cloud(captions)
+		image = wordcloud(captions)
 		image.save(filepath)
+
 	except ytcc.CouldNotRetrieveTranscript:
 		print("The specified video either doesn't exist or doesn't have captions enabled. Please try again")
 		exit()
-	except IOError:
-		print("There was an error saving the wordcloud file")
+	except IOError as e:
+		print(f"There was an error saving the wordcloud file: {e}")
 		exit()
 
 	return os.path.abspath(filepath)
