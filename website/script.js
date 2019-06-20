033c29cc6d9ea1cd17a0c68db386fbae78c89d12
Param Thakkar <pht0104@gmail.com>
2019-06-20 11:50:18 -0400

    Animate website down arrow with JavaScript

---
+++ website/script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    window.setTimeout(function(){
+        document.getElementById("arrow").style.opacity = "1";
+        document.getElementById("arrow").classList = "animated bounce";
+    }, 500)
+}
