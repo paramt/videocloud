74e0aad77d9fb379aa1a697906c0c4c5f9f6623c
Param Thakkar <pht0104@gmail.com>
2019-07-02 11:03:43 -0400

    Add custom filepath tests with JPEG and BMP extensions

--- tests/success.sh
+++ tests/success.sh
@@ -8,3 +8,9 @@ videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 1.png
 
 # Custom filepath (JPG)
 videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 2.jpg
+
+# Custom filepath (JPEG)
+videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 3.jpeg
+
+# Custom filepath (BMP)
+videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 4.bmp
