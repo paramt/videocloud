2e47a68996d3498241e31c9caf7226b34538b68f
Param Thakkar <pht0104@gmail.com>
2019-07-01 20:01:25 -0400

    Add custom filepath tests

--- tests/success.sh
+++ tests/success.sh
@@ -2,3 +2,9 @@
 
 # Defaults
 videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM
+
+# Custom filepath (PNG)
+videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 1.png
+
+# Custom filepath (JPG)
+videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 2.jpg
