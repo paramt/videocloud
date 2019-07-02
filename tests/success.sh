927c06dda0b9af0b3a3cab6cfb1b722ca91183b6
Param Thakkar <pht0104@gmail.com>
2019-07-02 11:10:43 -0400

    Add custom font tests

--- tests/success.sh
+++ tests/success.sh
@@ -20,3 +20,11 @@ videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 5.png en
 
 # Custom language code (Spanish)
 videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 6.png es
+
+# Custom font (Noto Sans)
+videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 7.png en \
+https://github.com/paramt/videocloud/blob/master/assets/fonts/NotoSans/NotoSans.ttf?raw=true
+
+# Custom font (Press Start 2P)
+videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 7.png en \
+https://github.com/paramt/videocloud/blob/master/assets/fonts/PressStart2P/PressStart2P-Regular.ttf\?raw=true
