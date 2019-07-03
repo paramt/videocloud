8ef11557b8594cf63cd4f2632a3296e9c96cdd72
Param Thakkar <pht0104@gmail.com>
2019-07-02 20:46:37 -0400

    Use variable to hold video link
    This way the video ID can easily be changed for all tests

--- tests/success.sh
+++ tests/success.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
+video=https://www.youtube.com/watch\?v=MHTizZ_XcUM
+
 # Defaults
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM
+videocloud $video
 
 # Custom filepath (PNG)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 1.png
+videocloud $video 1.png
 
 # Custom filepath (JPG)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 2.jpg
+videocloud $video 2.jpg
 
 # Custom filepath (JPEG)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 3.jpeg
+videocloud $video 3.jpeg
 
 # Custom filepath (BMP)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 4.bmp
+videocloud $video 4.bmp
 
 # Custom language code (English)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 5.png en
+videocloud $video 5.png en
 
 # Custom language code (Spanish)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 6.png es
+videocloud $video 6.png es
 
 # Custom font (Noto Sans)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 7.png en \
+videocloud $video 7.png en \
 https://github.com/paramt/videocloud/blob/master/assets/fonts/NotoSans/NotoSans.ttf?raw=true
 
 # Custom font (Press Start 2P)
-videocloud https://www.youtube.com/watch\?v=MHTizZ_XcUM 7.png en \
+videocloud $video 7.png en \
 https://github.com/paramt/videocloud/blob/master/assets/fonts/PressStart2P/PressStart2P-Regular.ttf\?raw=true
