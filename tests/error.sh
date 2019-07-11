64492cee371dd067c64e041e438ac8e23dd7c56b
Param Thakkar <pht0104@gmail.com>
2019-07-11 16:54:26 -0400

    Add "Bad video ID" test

--- tests/error.sh
+++ tests/error.sh
@@ -4,3 +4,10 @@ else
     echo "FAILED: No video ID provided"
     exit 1
 fi
+
+if videocloud --url=www.youtube.com | grep -q "The specified video either doesn't exist or doesn't have captions enabled."; then
+    echo "PASSED: Bad video ID provided"
+else
+    echo "FAILED: Bad video ID provided"
+    exit 1
+fi
